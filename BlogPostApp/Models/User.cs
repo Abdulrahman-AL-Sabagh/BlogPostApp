efe9d7109aab9f2fcc38c5b483aecde1dfbca407
Abdulrahman-AL-Sabagh <abdsabagh8@gmail.com>
2024-08-01 15:30:42 +0200

    add User Entity

---
+++ BlogPostApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogPostApp.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+    }
+}
